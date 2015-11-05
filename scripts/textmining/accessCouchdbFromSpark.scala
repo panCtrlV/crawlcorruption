35be65caff8ca9428dcc6a1b469bf64dc39912f9
Pan Chao <panchao25@gmail.com>
2015-11-05 16:03:54 -0500

    Added .gitignore

--- scripts/textmining/accessCouchdbFromSpark.scala
+++ scripts/textmining/accessCouchdbFromSpark.scala
@@ -1,5 +1,5 @@
 import org.apache.spark._
-import com.ibm.couchdb
+import com.ibm.couchdb._
 
 
 /**
@@ -8,3 +8,4 @@ import com.ibm.couchdb
  * Experimenting how to access CouchDB from Spark.
  */
 
+val couch = CouchDb("127.0.0.1", 5984)
